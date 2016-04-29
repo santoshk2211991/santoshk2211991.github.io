066f88da8140d1d73cbb3626c958081b98224a20
santoshk2211991 <santoshk2211991@gmail.com>
2016-04-30 01:28:07 +0530

    Added files via upload

---
+++ ammortization/amortization.js
@@ -0,0 +1,164 @@
+  'use strict';
+
+angular.module('myApp.amortization', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/amortization', {
+    templateUrl: 'ammortization/amortization.html',
+    controller: 'AmortizationCtrl'
+  });
+}])
+
+.controller('AmortizationCtrl', ['$scope',function($scope) {
+   $scope.val = '0'; 
+   $scope.interest ='';
+   $scope.loan = '';
+   $scope.months = '';
+   $scope.cusDur = '';
+   $scope.checked = false;
+   $scope.yearly = false;
+   $scope.half  = false;
+   $scope.quaterly = false;
+   $scope.customInp = false;
+
+   $scope.val4='';
+   $scope.formCount ='';
+   $scope.formMnth='';
+   $scope.formPrincipal='';
+
+$scope.custom = function(x) {
+    $scope.cusDur = x;
+    $scope.customInp = true;
+
+    if (x==1){
+/* $scope.yearly = true;
+   $scope.half  = false;
+   $scope.quaterly = false;
+   $scope.customInp = false;*/
+   $scope.val4=1;
+   $scope.range();
+   $scope.formMnth[0]=12;
+    }
+    else if(x==2){
+ /*$scope.yearly = false;
+   $scope.half  = true;
+   $scope.quaterly = false;
+   $scope.customInp = false;*/
+   $scope.val4=2;
+   $scope.range();
+   $scope.formMnth[0]=6;
+   $scope.formMnth[1]=12;
+
+    }
+    else if(x==3){
+        /*$scope.yearly = false;
+   $scope.half  = false;
+   $scope.quaterly = true; 
+   $scope.customInp = false;*/
+   $scope.val4=4;
+   $scope.range();
+   $scope.formMnth[0]=3;
+   $scope.formMnth[1]=6;
+   $scope.formMnth[2]=9;
+   $scope.formMnth[3]=12;
+    }
+    else if(x==4){
+      /*  $scope.yearly = false;
+   $scope.half  = false;
+   $scope.quaterly = false;
+   $scope.customInp = true;*/
+   $scope.val4=0;
+   $scope.range();
+    }
+     else{
+        /*$scope.yearly = false;
+   $scope.half  = false;
+   $scope.quaterly = false;
+   $scope.customInp = false;*/
+   $scope.customInp = false;
+    }
+
+}
+
+
+$scope.range = function() {
+        var n= $scope.val4;
+        console.log(typeof(n));
+        $scope.formCount= new Array(parseInt(n));
+        $scope.formMnth= new Array(parseInt(n));
+        $scope.formPrincipal= new Array(parseInt(n));
+    };
+
+$scope.compute1 = function() {
+
+    $scope.checked = true;
+
+    var i = ($scope.interest/12)/100;
+    var P = $scope.loan;
+    var n = $scope.months;
+    var exp = Math.pow((1+ i),n);
+
+    console.log('interest'+i+',loan'+P+',months'+n);
+    var amount = (i*P*exp)/(exp-1);
+    $scope.val = amount.toFixed(2);
+    console.log('$scope.val'+$scope.val);
+
+    $scope.totInt = 0;
+    $scope.list = [];
+
+    var tempBal = P;
+    var tempInt;
+    var tempPrincipal;
+    for(var j=0;j<n;j++){
+
+      var arr= {};
+      tempInt = parseFloat(tempBal)*parseFloat(i);
+      tempPrincipal = parseFloat(amount)-parseFloat(tempInt);
+
+      for (var k=0;k<$scope.val4;k++){
+        if(parseInt($scope.formMnth[k])==(j+1)){
+          console.log('found month '+ (j+1)+'amount'+$scope.formPrincipal[k])
+          tempPrincipal = parseFloat(tempPrincipal) +parseFloat($scope.formPrincipal[k]);
+          break;
+        }
+        else{/*flag=false;*/
+        }
+      }
+
+      tempBal = parseFloat(tempBal) - parseFloat(tempPrincipal);
+      
+      $scope.totInt = parseFloat($scope.totInt) + parseFloat(tempInt) ;
+      if (tempBal<0){
+
+        console.log('balance less than 0');
+
+        tempPrincipal=tempPrincipal+tempBal;
+        tempBal=0;
+
+        arr.interest = tempInt.toFixed(2);
+        arr.principal = tempPrincipal.toFixed(2);
+        arr.balance = tempBal.toFixed(2);
+        $scope.list.push(arr);
+
+        break;
+      }
+      else{
+        arr.interest = tempInt.toFixed(2);
+        arr.principal = tempPrincipal.toFixed(2);
+        arr.balance = tempBal.toFixed(2);
+
+        $scope.list.push(arr);
+      }
+     
+      console.log(i);
+    }
+
+
+    $scope.totAmt = parseFloat($scope.totInt) + parseFloat(P);
+    $scope.totAmt = $scope.totAmt.toFixed(2);
+    $scope.totInt = $scope.totInt.toFixed(2);
+    console.log($scope.list);
+ }
+}]);
+
+

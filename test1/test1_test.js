85e77663249c8a817d7790a1b351759b1e0644ed
santoshk2211991 <santoshk2211991@gmail.com>
2016-04-29 19:37:39 +0530

    Added files via upload

---
+++ test1/test1_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.amortization module', function() {
+
+  beforeEach(module('myApp.amortization'));
+
+  describe('amortization controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var amortizationCtrl = $controller('AmortizationCtrl');
+      expect(amortizationCtrl).toBeDefined();
+    }));
+
+  });
+});

994116ac5e4f72bcea0e4b6b1296d66348438ef2
santosh221991 <santosh221991@gmail.com>
2020-10-18 17:02:01 +0530

    VueJS Migration

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img :class = "spinnerClass" alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <Spinner /> -->
+  <Ammortization/>
+  
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <HelloWorld msg="Hello Vue 4.0 + Vite" /> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Spinner from './components/Spinner.vue';
+import Ammortization  from './components/Ammortization/Ammortization.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Spinner,
+    Ammortization
+  },
+  data() {
+    return {
+      spinnerClass: "test-color"
+    }
+  }
+}
+</script>
